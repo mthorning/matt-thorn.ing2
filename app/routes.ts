0e88c10293e93d3816ec15439c2c97e62fa709f9
Matt Thorning <matthewthorning@gmail.com>
2026-08-22 18:48:19 +0100

    working well

--- app/routes.ts
+++ app/routes.ts
@@ -12,5 +12,6 @@ export default [
     route('/posts/:slug', 'routes/posts/post.tsx'),
     route('/photos', 'routes/photos/photos.tsx'),
     route('/photos/:filename', 'routes/photos/photo.tsx'),
+    route('/minesweeper', 'routes/minesweeper/minesweeper.tsx'),
   ]),
 ] satisfies RouteConfig;
